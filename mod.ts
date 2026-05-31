d07e586494a0ea485309fd271d9390d387dd093d
Oscar Otero <oom@oscarotero.com>
2026-05-31 10:37:20 +0200

    improve remote files

--- mod.ts
+++ mod.ts
@@ -10,6 +10,6 @@ export default function (options: Partial<Options> = {}) {
     site.use(plugins(options));
 
     // Add the files needed for the theme
-    site.remote("/", import.meta.resolve("./src"), ["/**/*"]);
+    site.remote("/", import.meta.resolve("./src"), ["/**/*.{vto,scss,woff2,ts,svg,js,json}"]);
   };
 }
